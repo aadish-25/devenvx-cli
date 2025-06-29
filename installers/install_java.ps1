b301571898853287d580873d23826667e8779a62
Aadish Sonawane <aadishpcs@gmail.com>
2025-06-29 22:42:48 +0530

    added part inside install_java.ps1 that checks if java is already installed or not

--- installers/install_java.ps1
+++ installers/install_java.ps1
@@ -1,7 +1,40 @@
-Write-Output "==> Checking for Java..."
+<# Steps -
+    1. check if java is already installed, if yes then display message else install it
+    2. install java (jdk)
+    3. run it with silent flags
+    4. verify with java -version and javac -version
+    5. compile and run the hello.java script to confirm setup
+#>
 
-if (Get-Command javac -ErrorAction SilentlyContinue) {
-    Write-Output "[OK] Java is already installed."
+# Import reusable download progress loader
+. "$PSScriptRoot\..\cli\utils\showLoader.ps1"
+
+# checking if java is already installed
+Write-Host "`n[INFO] Checking if Java is already installed..." 
+
+$javaInstalled = $false
+$javacInstalled = $false
+
+try {
+    $javaVersion = java -version 2>&1
+    if ($javaVersion -match "version\s+`"\d+\.\d+") {
+        $javaInstalled = $true
+    }
+}
+catch {}
+
+try {
+    $javacVersion = javac -version 2>&1
+    if ($javacVersion -match "javac\s+\d+\.\d+") {
+        $javacInstalled = $true
+    }
+}
+catch {}
+
+if ($javaInstalled -and $javacInstalled) {
+    Write-Host "[SUCCESS] Java (JDK) is already installed." -ForegroundColor Green
+    Write-Host ""
+    exit 0
 } else {
-    Write-Output "[X] Java is NOT installed."
+    Write-Host "[FAIL] Java JDK is not fully installed." -ForegroundColor Red
 }

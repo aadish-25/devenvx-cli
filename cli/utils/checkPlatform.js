7e1d8b80a320a90db646071fe7549e9dbe3a6f6a
Aadish Sonawane <aadishpcs@gmail.com>
2025-07-01 16:55:51 +0530

    added proper comments, final commit

--- cli/utils/checkPlatform.js
+++ cli/utils/checkPlatform.js
@@ -1,11 +1,18 @@
+// Purpose of checkPlatform.js:
+// Verifies that the CLI is being executed on a supported platform (currently Windows only).
+// If the platform is not Windows, it displays an appropriate message and terminates execution.
+
 import os from 'os';
 import chalk from 'chalk';
 
+// Validates that the current OS is Windows ('win32'). Exits the process if not.
 export function ensureWindowsPlatform() {
   const platform = os.platform();
+  
+  // Display an error and exit if the platform is not supported (i.e., not Windows)
   if (platform !== 'win32') {
-    console.log(chalk.red('❌ Sorry! DevEnvx currently supports only Windows systems.'));
-    console.log(chalk.yellow('💡 macOS and Linux support is planned for future updates.\n'));
+    console.log(chalk.red('\n[FAIL] DevEnvx currently supports only Windows systems.'));
+    console.log(chalk.yellowBright('[INFO] macOS and Linux support is planned for future updates.\n'));
     process.exit(1);
   }
 }

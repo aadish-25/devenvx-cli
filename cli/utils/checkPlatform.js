d92863623d8ef7f3a8458be0c25d342f5e41a2df
Aadish Sonawane <aadishpcs@gmail.com>
2025-07-06 03:35:12 +0530

    added proper logs for mac and linux platforms

--- cli/utils/checkPlatform.js
+++ cli/utils/checkPlatform.js
@@ -10,9 +10,20 @@ export function ensureWindowsPlatform() {
   const platform = os.platform();
   
   // Display an error and exit if the platform is not supported (i.e., not Windows)
-  if (platform !== 'win32') {
+  if (platform === 'win32') {
     console.log(chalk.red('\n[FAIL] DevEnvx currently supports only Windows systems.'));
     console.log(chalk.yellowBright('[INFO] macOS and Linux support is planned for future updates.\n'));
+
+    if (platform === 'darwin') {
+      console.log(chalk.white('For macOS, you can use:'));
+      console.log(chalk.blueLight('• Homebrew: https://brew.sh'));
+    } else if (platform === 'linux') {
+      console.log(chalk.white('For Linux, you can use your distribution\'s package manager:'));
+      console.log(chalk.blueLight('• Ubuntu/Debian: apt install'));
+      console.log(chalk.blueLight('• Fedora/RHEL: dnf install'));
+    }
+
+    console.log()
     process.exit(1);
   }
 }

cf6074362893ed7af74228d9f72ce45c1fcdfbe8
Aadish Sonawane <aadishpcs@gmail.com>
2025-07-06 12:47:44 +0530

    fixed : 2 lines added instead of one after the help section in the main devenvx command screen and bumped to v1.0.7

--- cli/index.js
+++ cli/index.js
@@ -46,6 +46,6 @@ program
     .description("Show all supported languages")
     .action(handleList);
 
-program.addHelpText('afterAll', '\n');
+program.addHelpText('afterAll', '');
 
 program.parse(process.argv);
